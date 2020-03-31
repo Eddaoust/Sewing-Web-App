9129f13a85242e8c42d65f536b4b748f950faec6
Eddaoust <edmond.daoust@gmail.com>
2020-03-31 21:13:27 +0200

    Add MUI theming

--- src/index.js
+++ src/index.js
@@ -7,14 +7,38 @@ import thunkMiddleware from 'redux-thunk';
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducers/index'
+// Material UI Theming
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
+import './index.css';
 
 const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 
+const font = "'Comfortaa'";
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#2C58B5',
+        },
+        secondary: {
+            main: '#42CAA4',
+        }
+    },
+    typography: {
+        fontFamily: font,
+        h6: {
+            fontSize: 35,
+            fontWeight: "bold",
+        }
+    },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <App/>
-      </Provider>
+      <MuiThemeProvider theme={theme}>
+          <Provider store={store}>
+              <App/>
+          </Provider>
+      </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
