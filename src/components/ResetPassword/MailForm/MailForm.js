bf342728415818ecd3239f991c47070bc20a40c0
Eddaoust <edmond.daoust@gmail.com>
2020-04-22 08:31:12 +0200

    Handle error

--- src/components/ResetPassword/MailForm/MailForm.js
+++ src/components/ResetPassword/MailForm/MailForm.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Button, Container, Grid, TextField, Typography} from "@material-ui/core";
 import {Link as RouterLink} from "react-router-dom";
 import classes from '../../ResetPassword/MailForm/MailForm.module.css';
 import {useDispatch, useSelector} from "react-redux";
-import {sendMailProcess} from "../../../actions/resetPassword";
+import {sendMailProcess, sendMailClearError} from "../../../actions/resetPassword";
 
 const MailForm = (props) => {
     const dispatch = useDispatch();
     const resetPassword = useSelector(state => state.resetPassword);
+    // Clear Error on Component unmount
+    useEffect(() => {
+        return () => dispatch(sendMailClearError());
+    }, []);
 
-    //TODO Handle error
-    if (resetPassword.error) {
-
+    //Handle error
+    let error = false;
+    let helperText = '';
+    if (resetPassword.error && resetPassword.error.data === "Unable to find user") {
+        error = true;
+        helperText = 'Impossible de trouver un utilisateur lié à cette adresse email.';
+    } else if (resetPassword.error && resetPassword.error.data === "Unable to reset password from social media account") {
+        error = true;
+        helperText = 'Impossible de réinitialiser le mot de passe d\'un compte lié à un réseau social';
     }
 
     return (
@@ -29,6 +39,8 @@ const MailForm = (props) => {
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
                             <TextField
+                                error={error}
+                                helperText={helperText}
                                 variant="outlined"
                                 required
                                 fullWidth
